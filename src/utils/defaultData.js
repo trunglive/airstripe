8d78b2ad11a47c3217588f352b9148fac37d9d2b
trunglive <trunglive@gmail.com>
2018-03-30 23:04:36 -0400

    chore(util): create data api for view

---
+++ src/utils/defaultData.js
@@ -0,0 +1,218 @@
+export const defaultData = [
+  {
+    id: "wADHzRXpEI6ECPKwarwFd10P",
+    class: "economy",
+    destination: "Amsterdam",
+    title: "Flash Trip to Amsterdam",
+    description: "The unique travel experience",
+    totalSeat: 180,
+    currentBid: 50,
+    favorite: false,
+    airportCode: {
+      origin: "MAN",
+      destination: "AMS"
+    },
+    trip: {
+      depart: {
+        departureTime: 1543508820,
+        arrivalTime: 1543516020
+      },
+      return: {
+        departureTime: 1543761000,
+        arrivalTime: 1543770900
+      }
+    }
+  },
+  {
+    id: "DDTM7biFAC4efAFAdJNE10yH",
+    class: "business",
+    destination: "London",
+    title: "Visit London",
+    description: "The unique travel experience",
+    totalSeat: 180,
+    currentBid: 10,
+    favorite: false,
+    airportCode: {
+      origin: "BCN",
+      destination: "LGW"
+    },
+    trip: {
+      depart: {
+        departureTime: 1543922100,
+        arrivalTime: 1543932000
+      },
+      return: {
+        departureTime: 1544122800,
+        arrivalTime: 1544134200
+      }
+    }
+  },
+  {
+    id: "pD9nljmwlostJVC59pQXml91",
+    class: "first class",
+    destination: "Milan",
+    title: "Fun Grill Sizzle",
+    description: "The unique travel experience",
+    totalSeat: 180,
+    currentBid: 12,
+    favorite: false,
+    airportCode: {
+      origin: "MAD",
+      destination: "MIL"
+    },
+    trip: {
+      depart: {
+        departureTime: 1544240100,
+        arrivalTime: 1544249400
+      },
+      return: {
+        departureTime: 1544368800,
+        arrivalTime: 1544381100
+      }
+    }
+  },
+  {
+    id: "F2YWPJhzuLEGJkWeb8tDJzTa",
+    class: "economy",
+    destination: "Paris",
+    title: "Meet the city of lights",
+    description: "The unique travel experience",
+    totalSeat: 180,
+    currentBid: 17,
+    favorite: false,
+    airportCode: {
+      origin: "MAD",
+      destination: "CDG"
+    },
+    trip: {
+      depart: {
+        departureTime: 1544448000,
+        arrivalTime: 1544454300
+      },
+      return: {
+        departureTime: 1544737200,
+        arrivalTime: 1544748600
+      }
+    }
+  },
+  {
+    id: "nj1OZNMlTEqegFAq7jBpfyeX",
+    class: "business",
+    destination: "Seville",
+    title: "Fitness Flight",
+    description: "The unique travel experience",
+    totalSeat: 180,
+    currentBid: 90,
+    favorite: false,
+    airportCode: {
+      origin: "BCN",
+      destination: "SVQ"
+    },
+    trip: {
+      depart: {
+        departureTime: 1545218400,
+        arrivalTime: 1545227100
+      },
+      return: {
+        departureTime: 1545347700,
+        arrivalTime: 1545358800
+      }
+    }
+  },
+  {
+    id: "v7p7bPGULvHJK8sbPYd57MKP",
+    class: "first class",
+    destination: "London",
+    title: "Rockin the clouds",
+    description: "The unique travel experience",
+    totalSeat: 180,
+    currentBid: 97,
+    favorite: false,
+    airportCode: {
+      origin: "MAN",
+      destination: "LGW"
+    },
+    trip: {
+      depart: {
+        departureTime: 1545833100,
+        arrivalTime: 1545842400
+      },
+      return: {
+        departureTime: 1546009200,
+        arrivalTime: 1546022100
+      }
+    }
+  },
+  {
+    id: "xENCktqp0Sl73zYBWeXwXSqT",
+    class: "economy",
+    destination: "Paris",
+    title: "From the plane to the club",
+    description: "The unique travel experience",
+    totalSeat: 180,
+    currentBid: 107,
+    favorite: false,
+    airportCode: {
+      origin: "MAD",
+      destination: "CDG"
+    },
+    trip: {
+      depart: {
+        departureTime: 1545999000,
+        arrivalTime: 1546006800
+      },
+      return: {
+        departureTime: 1546267500,
+        arrivalTime: 1546281000
+      }
+    }
+  },
+  {
+    id: "Jv17IDGYjRIPe32odGoyRUP6",
+    class: "business",
+    destination: "London",
+    title: "Secret Sky Concert",
+    description: "The unique travel experience",
+    totalSeat: 180,
+    currentBid: 23,
+    favorite: false,
+    airportCode: {
+      origin: "BCN",
+      destination: "LGW"
+    },
+    trip: {
+      depart: {
+        departureTime: 1547469000,
+        arrivalTime: 1547487900
+      },
+      return: {
+        departureTime: 1547812800,
+        arrivalTime: 1547829600
+      }
+    }
+  },
+  {
+    id: "IaDCIoiKtnVXEMdSd73Y2ZwF",
+    class: "first class",
+    destination: "Rome",
+    title: "The Roman Flight",
+    description: "The unique travel experience",
+    totalSeat: 180,
+    currentBid: 52,
+    favorite: false,
+    airportCode: {
+      origin: "BCN",
+      destination: "FCO"
+    },
+    trip: {
+      depart: {
+        departureTime: 1548197400,
+        arrivalTime: 1548204900
+      },
+      return: {
+        departureTime: 1548724500,
+        arrivalTime: 1548732300
+      }
+    }
+  }
+];
