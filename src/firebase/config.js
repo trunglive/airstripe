fae521fe222817de6133a6f8e5cb84189c12797a
Trung Vo <trunglive@gmail.com>
2018-04-13 18:28:23 -0400

    Delete config.js

--- src/firebase/config.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-  apiKey: "AIzaSyAZ75zvHGfSfKck9qeYfNAEqNy-IPCn8Yo",
-  authDomain: "airstripe-dev.firebaseapp.com",
-  databaseURL: "https://airstripe-dev.firebaseio.com",
-  projectId: "airstripe-dev",
-  storageBucket: "airstripe-dev.appspot.com",
-  messagingSenderId: "1040227812347"
-};
